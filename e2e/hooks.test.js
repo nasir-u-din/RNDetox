69f3048b3c128ceaf0d5a9ccf872e7937c946a32
Nasir U Din <136651823+nasir-u-din@users.noreply.github.com>
2023-10-24 02:34:39 +0200

    react native starter hook modified

--- e2e/hooks.test.js
+++ e2e/hooks.test.js
@@ -1,5 +1,21 @@
-import { device } from 'detox';
+import { device, element, by, expect } from 'detox';
 
-beforeAll(async () => {
-  await device.launchApp();
+describe('Sample React Native App', () => {
+  beforeAll(async () => {
+    await device.launchApp();
+  });
+
+  it('should display the "Step One" section', async () => {
+    await expect(element(by.text('Step One'))).toBeVisible();
+    await expect(element(by.text('Edit App.tsx to change this screen and then come back to see your edits.'))).toBeVisible();
+  });
+
+  
+
+ 
+
+  it('should display the "Learn More" section', async () => {
+    await expect(element(by.text('Learn More'))).toBeVisible();
+    await expect(element(by.text('Read the docs to discover what to do next:'))).toBeVisible();
+  });
 });
--- e2e/suite1.test.js
+++
@@ -1,5 +0,0 @@
-import { device } from 'detox';
-
-it('should successfully sign up user with phone number', async () => {
-    await device.launchApp();
-  });
